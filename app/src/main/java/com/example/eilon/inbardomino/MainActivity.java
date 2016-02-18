d7e1c66785c03eb4196011d928fd43b302cfa474
eilonik <eilnoik@gmail.com>
2016-02-18 15:16:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/eilon/inbardomino/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.eilon.inbardomino;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private final int LEFT_IMAGE = -1;
+    private final int RIGHT_IMAGE = 1;
+    private boolean canSwipe = true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView dominoImage = (ImageView)findViewById(R.id.dominoImage);
+        dominoImage.setOnTouchListener(new OnSwipeTouchListener(MainActivity.this) {
+            public void onSwipeTop() {
+                //Toast.makeText(MainActivity.this, "top", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onSwipeRight() {
+                if(canSwipe) {
+                    canSwipe = false;
+                    rotateImage(dominoImage, RIGHT_IMAGE);
+                }
+            }
+
+            public void onSwipeLeft() {
+                if(canSwipe) {
+                    canSwipe = false;
+                    rotateImage(dominoImage, LEFT_IMAGE);
+                }
+            }
+
+            public void onSwipeBottom() {
+                //Toast.makeText(MainActivity.this, "bottom", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+    private void rotateImage(ImageView image, int direction) {
+        switch (direction) {
+            case LEFT_IMAGE:
+                image.setImageResource(R.mipmap.domino_down_left);
+                break;
+
+            case RIGHT_IMAGE:
+                image.setImageResource(R.mipmap.domino_down);
+                break;
+        }
+    }
+}
